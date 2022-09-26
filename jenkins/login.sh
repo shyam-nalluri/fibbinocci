394df7a12bf9b7af7b2338f250a85feb5a2bdbf6
shyamriversafe <113644831+shyamriversafe@users.noreply.github.com>
2022-09-26 14:34:33 -0700

    Added jekins pipeline files

--- jenkins/login.sh
+++ jenkins/login.sh
@@ -1,2 +1,2 @@
 echo 'logging in to docker hub'
-sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
